92b44e988ae4f0da94c150f373a94eac0c44f93d
julian <juliancano.dev@gmail.com>
2020-11-12 22:04:31 -0500

    first commit

---
+++ Assets/Scripts/ControlEnemigo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlEnemigo : MonoBehaviour
+{
+    public float vel = -1f;
+    Rigidbody2D rgb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 v = new Vector2(vel, 0);
+        rgb.velocity = v;
+    }
+}
